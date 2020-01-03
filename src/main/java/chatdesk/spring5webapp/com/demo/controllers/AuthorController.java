9d6fc65d83b5975b37c8a3e724b870869dae0182
Tichalo <ticha.muma@chatdesk.com>
2020-01-03 23:47:33 +0100

    added authors mappings

---
+++ src/main/java/chatdesk/spring5webapp/com/demo/controllers/AuthorController.java
@@ -0,0 +1,24 @@
+package chatdesk.spring5webapp.com.demo.controllers;
+
+import chatdesk.spring5webapp.com.demo.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model){
+
+        model.addAttribute("authors", authorRepository.findAll());
+
+        return "authors";
+    }
+}
--- src/main/java/chatdesk/spring5webapp/com/demo/controllers/BookController.java
+++ src/main/java/chatdesk/spring5webapp/com/demo/controllers/BookController.java
@@ -17,7 +17,8 @@ public class BookController {
     @RequestMapping("/books")
     public String getBooks(Model model){
 
-        model.addAttribute("book", bookRepository.findAll());
+        model.addAttribute("books", bookRepository.findAll());
+
         return "books";
     }
 }
